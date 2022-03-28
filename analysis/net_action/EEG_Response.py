83c866bffab6c90f9979f75941166ae2bba02eea
Vineet Tiruvadi <virati@gmail.com>
2022-03-28 12:13:00 -0700

    started porting to new dbspace

--- analysis/net_action/EEG_Response.py
+++ analysis/net_action/EEG_Response.py
@@ -9,28 +9,30 @@ Script to generate the EEG Response Violinplots
 """
 #%%
 
-from DBSpace.control import proc_dEEG
-from DBSpace.visualizations import EEG_Viz
+from dbspace.control import segmented_dEEG
+from dbspace.viz.MM import EEG_Viz
 
-import scipy.stats as stats
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set_context('paper')
+
+sns.set_context("paper")
 sns.set(font_scale=3)
-sns.set_style('white')
+sns.set_style("white")
 
 import pickle
 import cmocean
 
-pt_list = ['906','907','908']
+pt_list = ["906", "907", "908"]
 
-#The feature vector, in this case the frequencies
-fvect = np.linspace(0,500,513)
+# The feature vector, in this case the frequencies
+fvect = np.linspace(0, 500, 513)
 do_coherence = False
 
 ## Basic initialization methods, need to suppress figures from these and clean these up
-eFrame = proc_dEEG.proc_dEEG(pts=pt_list,procsteps='conservative',condits=['OnT','OffT'])
+eFrame = proc_dEEG.proc_dEEG(
+    pts=pt_list, procsteps="conservative", condits=["OnT", "OffT"]
+)
 eFrame.standard_pipeline()
 
 eFrame.band_distrs()
