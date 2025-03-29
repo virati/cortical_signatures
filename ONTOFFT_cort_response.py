38180ee63356d0ba98ae4590a9c145fb2ac455b9
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-03-28 19:44:56 -0700

    before trying 3.7 revert

--- ONTOFFT_cort_response.py
+++ ONTOFFT_cort_response.py
@@ -1,3 +1,4 @@
+# %%
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -7,40 +8,43 @@ Created on Wed Dec 25 15:54:53 2019
 Network Action - Compare ONT vs OFFT for EEG
 """
 
-from DBSpace.control import proc_dEEG
-import DBSpace as dbo
-from DBSpace.visualizations import EEG_Viz
-from DBSpace.control.TVB_DTI import DTI_support_model, plot_support_model
+from dbspace.control import proc_dEEG
+import dbspace as dbo
+from dbspace.visualizations import EEG_Viz
+from dbspace.control.TVB_DTI import DTI_support_model, plot_support_model
 
 import cmocean
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set_context('paper')
+
+sns.set_context("paper")
 sns.set(font_scale=3)
-sns.set_style('white')
-#%%
-pt_list = ['906','907','908']
-do_condits = ['OnT','OffT']
+sns.set_style("white")
+# %%
+pt_list = ["906", "907", "908"]
+do_condits = ["OnT", "OffT"]
 
 ## Basic initialization methods, need to suppress figures from these and clean these up
-eFrame = proc_dEEG.proc_dEEG(pts=pt_list,procsteps='conservative',condits=do_condits)
+eFrame = proc_dEEG.proc_dEEG(pts=pt_list, procsteps="conservative", condits=do_condits)
 eFrame.standard_pipeline()
 
-#%% PSD plotting
-eFrame.plot_psd(pt='907',condit='OffT',epoch='BOFT')
+# %% PSD plotting
+eFrame.plot_psd(pt="907", condit="OffT", epoch="BOFT")
 
-#%%
+# %%
 # Channel-marginalized Response Histogram
 eFrame.pop_meds(response=True)
 
-#%%
+# %%
 eFrame.band_distr()
 
-#%%
+# %%
 # Here we'll plot the spatial distributions of \alpha
-#for pt in pt_list:
-   
+# for pt in pt_list:
+
 #    eFrame.topo_median_response(do_condits=do_condits,pt=pt,band='Beta*')
-    
-#%%
-eFrame.topo_median_response(do_condits=do_condits,pt='908',band='Alpha',use_maya=True)
+
+# %%
+eFrame.topo_median_response(
+    do_condits=do_condits, pt="908", band="Alpha", use_maya=True
+)
