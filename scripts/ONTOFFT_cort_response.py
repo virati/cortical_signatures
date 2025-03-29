902d93447bc9cd4ce1f565a49858d22c29a5f30d
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-03-29 16:53:35 -0700

    most working, pyvista crashes

--- scripts/ONTOFFT_cort_response.py
+++ scripts/ONTOFFT_cort_response.py
@@ -46,7 +46,7 @@ for pt in pt_list:
 
 #    eFrame.topo_median_response(do_condits=do_condits,pt=pt,band='Beta*')
 
-# %%
-eFrame.topo_median_response(
-    do_condits=do_condits, pt="POOL", band="Beta*", use_maya=False
-)
+# %%%
+#eFrame.topo_median_response(
+#    do_condits=do_condits, pt="POOL", band="Beta*", use_maya=True
+#)
