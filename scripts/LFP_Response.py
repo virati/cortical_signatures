6349fbaa0845a2e3b3ae30f4e1676bc0adfb0742
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-03-28 23:20:14 -0700

    LFP violins working

---
+++ scripts/LFP_Response.py
@@ -0,0 +1,33 @@
+# %%
+%load_ext autoreload
+%autoreload 2
+
+import dbspace as dbo
+from dbspace import nestdict
+from dbspace.control import network_action
+
+import itertools
+from itertools import product as cart_prod
+
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import numpy as np
+
+import copy
+from copy import deepcopy
+
+# %%
+
+do_pts = ["901", "903", "905", "906", "907", "908"]
+analysis = network_action.local_response(do_pts=do_pts)
+analysis.extract_baselines()
+analysis.extract_response()
+analysis.gen_osc_distr()
+# %%
+analysis.plot_responses(do_pts=do_pts)
+
+
+# %%
+# analysis.plot_patient_responses()
+# %%
+analysis.plot_segment_responses(do_pts=do_pts)
