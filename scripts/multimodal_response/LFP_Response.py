6674dc43e2a1232a2fd0f89b0e69365409f85f9b
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-03-29 18:03:29 -0700

    cleanup imports

--- scripts/multimodal_response/LFP_Response.py
+++ scripts/multimodal_response/LFP_Response.py
@@ -2,24 +2,13 @@
 %load_ext autoreload
 %autoreload 2
 # Confirmed Working 3/29/2025
-
-import dbspace as dbo
-from dbspace import nestdict
 from dbspace.control import network_action
-
-import itertools
-from itertools import product as cart_prod
-
-import matplotlib.pyplot as plt
-import scipy.stats as stats
-import numpy as np
-
-import copy
-from copy import deepcopy
 
 # %%
 
 do_pts = ["901", "903", "905", "906", "907", "908"]
+
+#%%
 analysis = network_action.local_response(do_pts=do_pts)
 analysis.extract_baselines()
 analysis.extract_response()
