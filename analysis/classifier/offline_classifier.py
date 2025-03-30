151b36f7bc93baeafa829e068df8b7b3667847af
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-03-29 19:56:34 -0700

    classifier appears to work

--- analysis/classifier/offline_classifier.py
+++ analysis/classifier/offline_classifier.py
@@ -1,4 +1,6 @@
-#%%
+# %%
+%load_ext autoreload
+%autoreload 2
 """
 Created on Sun Jan 13 20:25:09 2019
 
@@ -7,27 +9,19 @@ clClassif Script
 Binary Classification for Cleaned EEG Data
 """
 
-#%%
+# %%
 from dbspace.control.offline_segments import network_action_dEEG
-import seaborn as sns
 
-
-#%%
-sns.set_context("paper")
-sns.set(font_scale=4)
-sns.set_style("white")
-
-
-#%%
+# %%
 all_pts = ["906", "907", "908"]
 
 EEG_analysis = network_action_dEEG(
     pts=all_pts,
     procsteps="conservative",
     condits=["OnT", "OffT"],
-    config_file="../configs/targeting_experiment.json",
+    config_file=None,
 )
-#%%
+# %%
 # Run the basic pipeline
 EEG_analysis.standard_pipeline()
 EEG_analysis.train_binSVM(mask=False)
@@ -37,5 +31,5 @@ EEG_analysis.bootstrap_binSVM()
 EEG_analysis.analyse_binSVM(feature_weigh=False)
 
 # EEG_analysis.OnT_dr(data_source=EEG_analysis.SVM_coeffs)
-#%%
+# %%
 EEG_analysis.learning_binSVM()
