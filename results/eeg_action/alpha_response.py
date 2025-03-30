e959196c44ed2574c95957209354136c9d81bbb2
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-03-29 21:51:31 -0700

    rpca working, good progress today!

--- results/eeg_action/alpha_response.py
+++ results/eeg_action/alpha_response.py
@@ -1,15 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 23 16:13:54 2018
+# %%
+# Confirmed works 3/29/2025
 
-@author: virati
-This scipt is focused on characterizing the ONTarget response
-Includes some DTI support modeling which should be split out
-
-"""
-
-#%%
+# %%
 
 from dbspace.control import offline_segments
 
@@ -19,7 +11,7 @@ import seaborn as sns
 sns.set_context("paper")
 sns.set(font_scale=3)
 sns.set_style("white")
-#%%
+# %%
 
 pt_list = ["906", "907", "908"]
 ## Basic initialization methods, need to suppress figures from these and clean these up
@@ -27,13 +19,13 @@ eFrame = offline_segments.network_action_dEEG(
     pts=pt_list,
     procsteps="conservative",
     condits=["OnT", "OffT"],
-    config_file="../configs/targeting_experiment.json",
+    config_file=None,
 )
 eFrame.standard_pipeline()
-#%% Generate the control modes
+# %% Generate the control modes
 # eFrame.OnT_ctrl_modes(pt='POOL') #THIS HAS BEEN MOVED TO control_modes.py
 
-#%% Plot the median response
+# %% Plot the median response
 eFrame.topo_median_response(
     do_condits=["OnT", "OffT"],
     pt="POOL",
@@ -44,7 +36,7 @@ eFrame.topo_median_response(
 )
 # eFrame.topo_median_variability(do_condits=['OnT','OffT'],pt='POOL',band='Alpha',use_maya=False)
 
-#%% Plot individual patients
+# %% Plot individual patients
 for pp in pt_list:
     eFrame.topo_median_response(
         do_condits=["OnT", "OffT"],
