29742e3a274737c2778a1167802d05ab1b7e0268
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-04-29 15:01:31 -0700

    liberal not working, try to fix next

--- scripts/SVM/clEEG_classif_script.py
+++ scripts/SVM/clEEG_classif_script.py
@@ -14,7 +14,7 @@ sns.set_style("white")
 all_pts = ["906", "907", "908"]
 
 EEG_analysis = proc_dEEG.proc_dEEG(
-    pts=all_pts, procsteps="liberal", condits=["OnT", "OffT"]
+    pts=all_pts, procsteps="conservative", condits=["OnT", "OffT"]
 )
 # %%
 EEG_analysis.standard_pipeline()
