b1903c0c2e4f47e2cf2ca680f89d140df635d386
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-05-11 21:07:37 -0700

    more DO cleanup

--- scripts/engaged_tractography/Viz_DO_DTI.py
+++ scripts/engaged_tractography/Viz_DO_DTI.py
@@ -10,45 +10,45 @@ import numpy as np
 import itertools
 import json
 
-
-with open("../../assets/experiments/metadata/mayberg_900S_electrode_map.json") as electrode_map_path:
-    Etrode_map = json.load(electrode_map_path)
+#%%
 BASE_DATA_DIR = "/home/virati/Data/phd_vrt_2013/neural/imaging/DTI/MDT_DBS_2_7V_Tractography/" #NEEDS TO BE SHIFTED TO JSON
+electrode_map_path = "../../assets/experiments/metadata/mayberg_900S_electrode_map.json"
+DO_map_path = "../../assets/experiments/DOs/mayberg_900S_DO_map.json"
 
 #%%
-all_pts = ['901','903','905','906','907','908']
-all_condits = ['OnT','OffT']
-all_sides = ['L','R','L+R']
-voltage = '3'
+# NEED TO BUILD CONFIG LOAD HELPER FUNCTIONS HERE TODO
 
+with open(electrode_map_path) as electrode_map_file:
+    Etrode_map = json.load(electrode_map_file)
+with open(DO_map_path) as DO_map_file:
+    DO_map = json.load(DO_map_file)
+
+all_pts = DO_map['parameters']['subjects']
+all_condits = DO_map['parameters']['conditions']
+all_sides = DO_map['parameters']['sides']
 DO_all = itertools.product(all_pts,all_condits,all_sides)
-DO_positive = [('901','OnT','L'),
-               ('901','OnT','L+R'),
-               ('901','OffT','L'),
-               ('901','OffT','L+R'),
-               ('903','OffT','L'),
-               ('903','OffT','L+R'),
-               ('905','OnT','R'),
-               ('905','OnT','L+R'),
-               ('905','OffT','R'),
-               ('905','OffT','L+R'),
-               ('906','OffT','R'),
-               ('906','OffT','L+R')] #This reflects the STIM conditions that evoked DOs
-
+DO_positive = [(item["subject"], item["condition"], item["side"]) for item in DO_map['DO_positive']]
 DO_negative = [x for x in DO_all if x not in DO_positive]
+
+
+#%%
+sim_voltage = '2'
+
 
 dti_file = nestdict()
 data = nestdict()
 tractos = nestdict()
-
-data_arr = np.zeros((6,2,2,182,218,182))
 combined = nestdict()
+
+image_dim = (182,218,182)
+
+data_arr = np.zeros((len(all_pts),len(all_condits),len(all_sides),*image_dim)) #TODO direct references instead of floating dims
 
 for pp,pt in enumerate(all_pts):
     for cc,condit in enumerate(['OnT','OffT']):
         for ss,side in enumerate(['L','R']):
             cntct = Etrode_map[condit][pt][ss]+1
-            dti_file[pp][condit][side] = BASE_DATA_DIR + 'DBS'+str(pt) + '.'+side+str(cntct)+'.' + voltage + 'V.bin.nii.gz'
+            dti_file[pp][condit][side] = BASE_DATA_DIR + 'DBS'+str(pt) + '.'+side+str(cntct)+'.' + sim_voltage + 'V.bin.nii.gz'
         
             tractos[pt][condit][side] = image.smooth_img(dti_file[pp][condit][side],fwhm=1)
 
@@ -57,14 +57,12 @@ for pp,pt in enumerate(all_pts):
 
         tractos[pt][condit]['L+R'] = image.math_img("img1+img2",img1=tractos[pt][condit]['L'],img2=tractos[pt][condit]['R'])
 #%%
+do_pos = nestdict()
 img = [None] * len(DO_positive)
 do_pos_string = ''
 for aa,amalg in enumerate(DO_positive):
     img[aa] = tractos[DO_positive[aa][0]][DO_positive[aa][1]][DO_positive[aa][2]]
     do_pos_string += 'img' + str(aa) + ','
-    
-do_pos = nestdict()
-
 iter_do_pos = {'img'+str(num):img[num] for num in range(len(DO_positive))}
 
 do_pos[condit] = image.math_img("np.mean(np.array(["+do_pos_string+"]),axis=0)",**iter_do_pos)
