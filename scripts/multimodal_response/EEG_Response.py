5547772995758ac57f9b9dac2d64ea761cb5462f
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-10-21 08:47:02 -0700

    sliding window gif code working

--- scripts/multimodal_response/EEG_Response.py
+++ scripts/multimodal_response/EEG_Response.py
@@ -9,6 +9,13 @@ from dbspace.control import proc_dEEG
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+from PIL import Image
+import tempfile
+
+import numpy as np
+from matplotlib.animation import FuncAnimation, PillowWriter
+
 
 sns.set_context("paper")
 sns.set(font_scale=2)
@@ -35,21 +42,16 @@ eFrame.pop_meds(response=True,pt='POOL', seg_lim=(0,10))
 eFrame.band_distr(do_moment="mads")
 
 # %%%
-import numpy as np
-from matplotlib.animation import FuncAnimation, PillowWriter
+
 max_segments = np.min([len(eFrame.osc_bl_norm_timeidx[pt]['OnT']) for pt in pt_list])
-sliding_windows = [(0,5),(2,7),(4,9),(6,11),(8,13),(10,15),(12,17),(14,19),(16,21),(18,23),(20,25),(22,27),(24,29),(26,31),(28,33),(30,35),(32,37),(34,39),(36,41),(38,43),(40,45),(42,47),(44,49),(46,51),(48,53),(50,55)]
+#sliding_windows = [(0,5),(2,7),(4,9),(6,11),(8,13),(10,15),(12,17),(14,19),(16,21),(18,23),(20,25),(22,27),(24,29),(26,31),(28,33),(30,35),(32,37),(34,39),(36,41),(38,43),(40,45),(42,47),(44,49),(46,51),(48,53),(50,55)]
+window_size = 2
+sliding_windows = [(i, i + window_size) for i in range(0, max_segments - window_size + 1)]
 for seg_lim in sliding_windows:
     eFrame.topo_median_response(
         do_condits=['OnT'], pt="POOL", band="Alpha", use_maya=False, seg_lim=seg_lim
     )
     plt.suptitle(f"Segment Window: {seg_lim[0]}-{seg_lim[1]}")
-
-#%%
-# Create animated GIF from the sliding window plots
-import os
-from PIL import Image
-import tempfile
 
 # Create a temporary directory to store individual frames
 temp_dir = tempfile.mkdtemp()
