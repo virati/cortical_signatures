2a6e23618d7b54d1400745cf5935b85ef981a2a4
Vineet Tiruvadi <virati@gmail.com>
2026-06-23 06:42:11 +0000

    Use BONT/BOFFT/LONT/LOFFT/RONT/ROFFT condition keys; compare each config vs own and bilateral OffT

--- analyses/response_modes.py
+++ analyses/response_modes.py
@@ -13,22 +13,28 @@ sns.set(font_scale=1)
 sns.set_style("white")
 # %%
 pt_list = ["906", "907", "908"]
-do_condits = ["OnT", "OffT", "LeftT", "RightT"]
 
-## Basic initialization methods, need to suppress figures from these and clean these up
+configurations = {
+    "Bilateral": ("BONT", "BOFFT"),
+    "Left":      ("LONT", "LOFFT"),
+    "Right":     ("RONT", "ROFFT"),
+}
+do_condits = [c for pair in configurations.values() for c in pair]
+stim_on_condits = [ont for ont, _ in configurations.values()]
+
 eFrame = proc_dEEG.proc_dEEG(pts=pt_list, procsteps="conservative", condits=do_condits)
 eFrame.standard_pipeline(blank_out_gamma=False)
 
 #%%
-for band in ['Alpha','Beta*']:
-    for condit in ['OnT', 'LeftT', 'RightT']:
+for band in ['Alpha', 'Beta*']:
+    for condit in stim_on_condits:
         eFrame.topo_median_response(do_condits=[condit], band=band, render_3d=True, write_output='/tmp/cort_response/')
 
 #%%
-for condit in ['OnT', 'LeftT', 'RightT']:
+for condit in stim_on_condits:
     eFrame.topo_OnT_actionmode(pt='POOL', do_plot=True, render_3d=True, do_condits=[condit])
 
 #%%
-# Tracks channels that 'change together' per stimulation laterality
-for condit in ['OnT', 'LeftT', 'RightT']:
+# Tracks channels that 'change together' per stimulation configuration
+for condit in stim_on_condits:
     eFrame.topo_OnT_alpha_ctrl(pt='POOL', do_plot=True, band='Alpha', do_condits=[condit])
